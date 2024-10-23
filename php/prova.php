0c604a446e79f0537a5e519986847a195e948ed1
Lluis <lluismut@gmail.com>
2024-10-23 16:36:10 +0200

    first commit

---
+++ php/prova.php
@@ -0,0 +1,13 @@
+<html>
+    <body>
+    
+        <?php 
+        $llista =["nom" => "Pomes", "preu" => "6", "quantitat" => "2"]
+        ["nom" => "Platanos", "preu" => "10", "quantitat" => "8"]
+        ["nom" => "Kiwis", "preu" => "8", "quantitat" => "5"]
+        ["nom" => "Freses", "preu" => "2", "quantitat" => "12"]
+        ?>
+
+    </body> 
+</html>
+
